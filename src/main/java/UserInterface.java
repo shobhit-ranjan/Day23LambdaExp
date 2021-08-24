0b7ffd50b46f68e79a56b54cccde26d2dc6c01e4
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-24 18:31:47 +0530

    first name

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,7 @@
+
+public interface UserInterface {
+
+	public Boolean UserData(String data);
+
+	
+}
---
+++ src/test/java/TestuserInput.java
@@ -0,0 +1,9 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestuserInput {
+
+	userData data = new userData();
+
+}
